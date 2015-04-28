f051803fd972a1b0e6b70b5a4a0d1b99b70fb1ef
Alexey Fansky <alexey.fansky@effective-soft.com>
2015-04-27 17:05:14 -0700

    Displaying star in the star system (#4)

--- Assets/Scripts/StarLoadScript.cs
+++ Assets/Scripts/StarLoadScript.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class StarLoadScript : MonoBehaviour {
 
 	public Transform planet;
+	public Transform starPrefab;
 
 	// Use this for initialization
 	void Start () {
 		Star star = GameContext.CurrentStar;
+		Instantiate (starPrefab, new Vector3 (0, 0, 0), Quaternion.identity);
+
 		foreach (Planet planetModel in star.planets) {
 			Transform planetObject = Instantiate (planet, new Vector3 (planetModel.x, planetModel.y, 0), Quaternion.identity) as Transform;
 			planetObject.gameObject.GetComponent<PlanetScript>().planet = planetModel;
